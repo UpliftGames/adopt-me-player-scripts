72b1e25c742c85602a0332f0978d235bd60d9d79
Tansen Zhu <tansen@uplift.games>
2023-05-04 08:13:03 -0700

    Make the replace code agnostic to whatever is found in ReplaceFiles

--- src/Modifiers/AdoptMe/init.lua
+++ src/Modifiers/AdoptMe/init.lua
@@ -1,11 +1,24 @@
 local ReplaceFiles = script:WaitForChild("ReplaceFiles")
-local NewPopper = ReplaceFiles.Popper
 
--- iterate replacefiles children
+local filesToReplace = {}
+local function findFilesToReplace(parent, leaves)
+    for _, child in ipairs(parent:GetChildren()) do
+        if #child:GetChildren() == 0 then
+            table.insert(leaves, child)
+        else
+            findFilesToReplace(child, leaves)
+        end
+    end
+end
+findFilesToReplace(ReplaceFiles, filesToReplace)
 
 return function(PlayerModule)
-	--replace all listed files
-
-	PlayerModule.CameraModule.ZoomController.Popper:Destroy()
-	NewPopper:Clone().Parent  = PlayerModule.CameraModule.ZoomController
+	for _, file in ipairs(filesToReplace) do
+		local target = PlayerModule.CameraModule:FindFirstChild(file.Name, true) or PlayerModule.ControlModule:FindFirstChild(file.Name, true)
+		if target then
+			local targetParent = target.Parent
+			target:Destroy()
+			file:Clone().Parent = targetParent
+		end
+	end
 end
