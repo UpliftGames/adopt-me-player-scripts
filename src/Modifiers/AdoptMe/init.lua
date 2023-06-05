f66afd8ffad8219b3a2e0d2eafed9026012445c8
Tansen Zhu <tansen@uplift.games>
2023-06-05 09:45:35 -0700

    WIP with bugs

--- src/Modifiers/AdoptMe/init.lua
+++ src/Modifiers/AdoptMe/init.lua
@@ -1,10 +1,20 @@
 local ReplaceFiles = script:WaitForChild("ReplaceFiles")
 
+local function get_instance_path(instance)
+    local path = {}
+	table.insert(path, 1, instance.Name)
+    while instance.Parent ~= nil and instance.Name ~= "CameraModule" and instance.Name ~= "ControlModule" do
+        instance = instance.Parent
+		table.insert(path, 1, instance.Name)
+    end
+    return path
+end
+
 local filesToReplace = {}
 local function findFilesToReplace(parent, leaves)
     for _, child in ipairs(parent:GetChildren()) do
         if #child:GetChildren() == 0 then
-            table.insert(leaves, child)
+            table.insert(leaves, {file=child, path=get_instance_path(child)})
         else
             findFilesToReplace(child, leaves)
         end
@@ -13,12 +23,12 @@ end
 findFilesToReplace(ReplaceFiles, filesToReplace)
 
 return function(PlayerModule)
-	for _, file in ipairs(filesToReplace) do
-		local target = PlayerModule.CameraModule:FindFirstChild(file.Name, true) or PlayerModule.ControlModule:FindFirstChild(file.Name, true)
-		if target then
-			local targetParent = target.Parent
-			target:Destroy()
-			file:Clone().Parent = targetParent
-		end
-	end
+	-- for _, file in ipairs(filesToReplace) do
+	-- 	local target = PlayerModule.CameraModule:FindFirstChild(file.Name, true) or PlayerModule.ControlModule:FindFirstChild(file.Name, true)
+	-- 	if target then
+	-- 		local targetParent = target.Parent
+	-- 		target:Destroy()
+	-- 		file:Clone().Parent = targetParent
+	-- 	end
+	-- end
 end
