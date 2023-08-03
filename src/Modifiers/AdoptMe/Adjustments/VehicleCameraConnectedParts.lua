056ac8e6c8cc88f62a302a33af2833a0687a5638
Tansen Zhu <tansen@uplift.games>
2023-08-03 08:33:39 -0700

    VehicleCamera get connected parts patch

---
+++ src/Modifiers/AdoptMe/Adjustments/VehicleCameraConnectedParts.lua
@@ -0,0 +1,51 @@
+--!nonstrict
+
+local module = {}
+
+module.priority = 1
+
+function module.apply(PlayerModule: ModuleScript)
+	local cameraModule = PlayerModule:WaitForChild("CameraModule")
+	local vehicleCamera = require(cameraModule:WaitForChild("VehicleCamera"))
+    local VehicleCameraCore = require(vehicleCamera:WaitForChild("VehicleCameraCore"))
+    local VehicleCameraConfig = require(vehicleCamera:WaitForChild("VehicleCameraConfig"))
+    local CameraUtils = require(cameraModule:WaitForChild("CameraUtils"))
+
+    local Spring = CameraUtils.Spring
+
+    local EPSILON = 1e-3
+    local YAW_DEFAULT = math.rad(0)
+
+    function vehicleCamera:Reset()
+		self.vehicleCameraCore = VehicleCameraCore.new(self:GetSubjectCFrame())
+        self.pitchSpring = Spring.new(0, -math.rad(VehicleCameraConfig.pitchBaseAngle))
+        self.yawSpring = Spring.new(0, YAW_DEFAULT)
+        self.lastPanTick = 0
+        
+        local camera = workspace.CurrentCamera
+        local cameraSubject = camera and camera.CameraSubject
+        
+        assert(camera)
+        assert(cameraSubject)
+        assert(cameraSubject:IsA("VehicleSeat"))
+        
+        -- GetConnectedParts is not coming back with the list we expect, so do this instead
+        local assemblyParts = {}
+        local parentModel = cameraSubject.Parent
+        for _, part in parentModel:GetDescendants() do
+            if part:IsA("BasePart") then
+                table.insert(assemblyParts, part)
+            end
+        end
+        local assemblyPosition, assemblyRadius = CameraUtils.getLooseBoundingSphere(assemblyParts)
+        
+        assemblyRadius = math.max(assemblyRadius, EPSILON)
+        
+        self.assemblyRadius = assemblyRadius
+        self.assemblyOffset = cameraSubject.CFrame:Inverse()*assemblyPosition -- seat-space offset of the assembly bounding sphere center
+        
+        self:_StepInitialZoom()
+	end
+end
+
+return module
